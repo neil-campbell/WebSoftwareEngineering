0038b05176aa7ae69da23969319c60ae6eac35dd
Neil Campbell <n-campbell@cardiac-services.com>
2019-04-25 10:03:30 +0100

    pulling data from DB and displaying it in a table

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Orders;
 use Symfony\Component\HttpFoundation\Response; 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class OrdersController extends AbstractController
 {
@@ -55,6 +56,20 @@ class OrdersController extends AbstractController
         return new Response(
             'A new Order has been added.'
         );
+    }
+
+     /**
+     * @Route("/order/getOrders", name="getOrders") methods={"GET"}
+     */
+    public function getOrders(){
+        $repo = $this->getDoctrine()->getRepository(Orders::class); // the type of the entity
+        $orders = $repo->findAll();
+        if (!$orders) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+        return new JsonResponse($orders);     
     }
 }
     
